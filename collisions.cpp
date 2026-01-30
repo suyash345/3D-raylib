e154d45bf1c3c0871b6b91baa1d795dc64327aae
Suyash Khanna <suyashkhanna345@gmail.com>
2026-01-30 21:59:34 +1030

    added more stuff

--- collisions.cpp
+++ collisions.cpp
@@ -2,6 +2,13 @@
 #include <raymath.h>
 #include <iostream>
 
+BoundingBox GetUpdatedBoundingBox(Model model, Vector3 pos) {
+	BoundingBox box = GetMeshBoundingBox(model.meshes[0]);
+	Vector3 min_bound = Vector3(pos + box.min);
+	Vector3 max_bound = Vector3(pos + box.max);
+	return BoundingBox{ min_bound, max_bound };
+}
+
 int main() {
 	InitWindow(1920, 1080, "collisions");
 	Camera3D camera;
@@ -11,10 +18,11 @@ int main() {
 	camera.fovy = 45;
 	camera.projection = CAMERA_PERSPECTIVE;
 
-	Model player = LoadModelFromMesh(GenMeshSphere(0.5, 12, 12));
+	Model player = LoadModelFromMesh(GenMeshCube(1,1,1));
 	Vector3 pos = {0.0f,0.0f,0.0f};
 	Vector3 direction = {0.0f,0.0f,0.0f};
 	float speed = 5;
+	BoundingBox bbox = GetMeshBoundingBox(player.meshes[0]);
 
 	Model obstacle = LoadModelFromMesh(GenMeshSphere(0.5, 12, 12));
 	Vector3 obstacle_pos = { 3,0,0 };
@@ -27,8 +35,8 @@ int main() {
 
 		pos.x+= direction.x * speed * delta_time;
 		pos.z+= direction.z * speed * delta_time;
-
-		std::cout << pos.x << pos.z << std::endl;
+		
+		std::cout << CheckCollisionBoxSphere(GetUpdatedBoundingBox(player,pos), obstacle_pos, 2);
 
 		BeginDrawing();
 		ClearBackground(WHITE);
